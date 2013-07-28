a203ee2b21fbec8520c57bd918af75b96050325c
Alex Poms <alexpoms@gmail.com>
2013-07-27 17:41:28 -0700

    Add Problems 1 & 2

--- src/clj_euler/core.clj
+++ src/clj_euler/core.clj
@@ -1,4 +1,18 @@
-(ns clj-euler.core)
+(ns clj-euler.core
+  [:require [clojure set]] )
+
+(defn p1 []
+  (let [mrange #(set (range 0 1000 %))]
+    (apply + (clojure.set/union (mrange 3) (mrange 5)))))
+
+(defn fib-seq
+  ([]
+     (fib-seq 1 1))
+  ([a b]
+     (cons a (lazy-seq (fib-seq b (+ b a))))))
+
+(defn p2 []
+  (apply + (filter even? (take-while (partial > 4000000) (fib-seq)))))
 
 (defn foo
   "I don't do a whole lot."
